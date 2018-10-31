fdfbba4ac72b08ec8ce13b578d46fe6c519d7a17
Walter Longo Neto <walter.longo@trackmob.com.br>
2018-10-31 18:34:16 -0300

    Fix lint issues

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react' // eslint-disable-line no-unused-vars
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { render } from 'react-dom'
 import reducers from './app/reducers'
 import rootSaga from './app/sagas'
-import App from './app/App'
+import App from './app/App' // eslint-disable-line no-unused-vars
 
-import boostrap from 'bootstrap'
-import $ from 'jquery'
+import boostrap from 'bootstrap' // eslint-disable-line no-unused-vars
+import $ from 'jquery' // eslint-disable-line no-unused-vars
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './app/styles.css'
 
@@ -21,5 +21,5 @@ sagaMiddleware.run(rootSaga)
 
 render(
   <App store={store} />,
-  document.getElementById('root')
+  document.getElementById('root') // eslint-disable-line no-undef
 )
